e3ee5a68c004e240f4747f7170ccb91c72202210
Chaitanya N <114483098+Chaitu-1654@users.noreply.github.com>
2022-11-07 06:51:46 +0000

    solved

--- set01/problem06.c
+++ set01/problem06.c
@@ -1,3 +1,28 @@
+#i
 int input();
 void compare(int a, int b, int c, int *largest);
 void output(int a, int b, int c, int largest);
+
+int main() {
+  int a,b,c,l;
+  a = input();
+  b = input();
+  c = input();
+  compare(a,b,c,&1);
+  output(a,b,c,l);
+  }
+
+int input() {
+  int x;
+  printf("enter the number\n");
+  scanf("d", &x);
+  return(x);
+  }
+vold compare( int a, int b, int c, int *largest){
+  if(a>b)
+  {
+  if (a>c){ largest = a;}
+  else{ largest = c;}
+    }
+  else
+    
